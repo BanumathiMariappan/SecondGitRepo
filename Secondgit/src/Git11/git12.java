3d9a5739e5290dd43fd87697f601dd5e3605f2c2
banumathi <banumathi.m@fourbends.com>
2017-08-17 11:27:52 +0530

    initial changes

--- Secondgit/src/Git11/git12.java
+++ Secondgit/src/Git11/git12.java
@@ -4,7 +4,7 @@ public class git12 {
 	public static void main(String [] args){
 		
 		System.out.println("Good Evening Anu");
-		System.out.println("Good Night");
+		System.out.println("Good Night Jaya");
 		}
 		
 }
